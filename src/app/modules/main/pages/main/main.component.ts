c0972113f7fa2cb55a1d4ecdeb9a439c2f2aa58f
Taras <tarasdemidovich1995@gmail.com>
2020-12-10 12:05:38 +0300

    feat: add menu navigation

--- src/app/modules/main/pages/main/main.component.ts
+++ src/app/modules/main/pages/main/main.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { KEY_CODE } from 'src/app/shared/enums/key-codes.enum';
+import { MenuItem } from 'src/app/modules/main/shared/interfaces';
 
 @Component({
   selector: 'app-main',
@@ -6,11 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-  public menuPoints = [
-    'New game',
-    'Settings',
-    'How to play',
-    'Statistics'
+  public menuPoints: MenuItem[] = [
+    { name: 'New game', path: '/game' },
+    { name: 'Settings', path: '/main/settings' },
+    { name: 'How to play', path: '/main/description' },
+    { name: 'Statistics', path: '/main/statistics' }
   ];
-  public currentPoint = 2;
+  public currentPoint = 0;
+
+  constructor(private router: Router) {}
+
+  @HostListener('window:keyup', ['$event'])
+  public keyEvent(event: KeyboardEvent): void {
+    switch (event.key) {
+      case KEY_CODE.DOWN_ARROW:
+        this.currentPoint = (this.currentPoint + 1) % 4;
+        break;
+      case KEY_CODE.UP_ARROW:
+        this.currentPoint = this.currentPoint
+          ? this.currentPoint - 1
+          : this.menuPoints.length - 1;
+        break;
+      case KEY_CODE.ENTER:
+        this.router.navigate([this.menuPoints[this.currentPoint].path]);
+        break;
+      default:
+        break;
+    }
+  }
 }
---
+++ src/app/shared/enums/key-codes.enum.ts
@@ -0,0 +1,7 @@
+export enum KEY_CODE {
+  RIGHT_ARROW = 'ArrowRight',
+  LEFT_ARROW = 'ArrowLeft',
+  DOWN_ARROW = 'ArrowDown',
+  UP_ARROW = 'ArrowUp',
+  ENTER = 'Enter'
+}
